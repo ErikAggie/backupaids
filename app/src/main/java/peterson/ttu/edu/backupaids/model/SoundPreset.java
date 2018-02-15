0e45edd741e4c14278521135ecdbc965abe2637b
ErikAggie <erikaggie02@gmail.com>
2018-02-15 09:24:28 -0600

    Initial work to load and populate presets.

---
+++ app/src/main/java/peterson/ttu/edu/backupaids/model/SoundPreset.java
@@ -0,0 +1,183 @@
+package peterson.ttu.edu.backupaids.model;
+
+import android.util.JsonReader;
+
+import java.io.IOException;
+
+/**
+ * Holds a preset
+ */
+
+public class SoundPreset {
+    // Key names in the Json file
+    private static final String PRESET_NAME = "PresetName";
+    private static final String VOLUME_LEVEL = "VolumeLevel";
+    private static final String HZ125 = "Hz125";
+    private static final String HZ250 = "Hz250";
+    private static final String HZ500 = "Hz500";
+    private static final String HZ1000 = "Hz1000";
+    private static final String HZ2000 = "Hz2000";
+    private static final String HZ3000 = "Hz3000";
+    private static final String HZ4000 = "Hz4000";
+    private static final String HZ8000 = "Hz8000";
+
+
+    private String mName;
+    private int mVolumeAdjust;
+    private short mHz125;
+    private short mHz250;
+    private short mHz500;
+    private short mHz1000;
+    private short mHz2000;
+    private short mHz3000;
+    private short mHz4000;
+    private short mHz8000;
+
+    /**
+     * Constructor from Json. Package-private since only SoundPresetManager
+     * should call it.
+     */
+    SoundPreset(JsonReader jsonReader) throws IOException {
+        jsonReader.beginObject();
+        while ( jsonReader.hasNext()) {
+            switch ( jsonReader.nextName()) {
+                case PRESET_NAME:
+                    mName = jsonReader.nextString();
+                    break;
+                case VOLUME_LEVEL:
+                    mVolumeAdjust = jsonReader.nextInt();
+                    break;
+                case HZ125:
+                    mHz125 = (short)jsonReader.nextInt();
+                    break;
+                case HZ250:
+                    mHz250 = (short)jsonReader.nextInt();
+                    break;
+                case HZ500:
+                    mHz500 = (short)jsonReader.nextInt();
+                    break;
+                case HZ1000:
+                    mHz1000 = (short)jsonReader.nextInt();
+                    break;
+                case HZ2000:
+                    mHz2000 = (short)jsonReader.nextInt();
+                    break;
+                case HZ3000:
+                    mHz3000 = (short)jsonReader.nextInt();
+                    break;
+                case HZ4000:
+                    mHz4000 = (short)jsonReader.nextInt();
+                    break;
+                case HZ8000:
+                    mHz8000 = (short)jsonReader.nextInt();
+                    break;
+                default:
+                    throw new RuntimeException("Unknown Json parameter " + jsonReader.nextName());
+            }
+        }
+        jsonReader.endObject();
+    }
+
+    public SoundPreset(String name,
+                       int volumeAdjust,
+                       short hz125,
+                       short hz250,
+                       short hz500,
+                       short hz1000,
+                       short hz2000,
+                       short hz3000,
+                       short hz4000,
+                       short hz8000) {
+        this.mName = name;
+        this.mVolumeAdjust = volumeAdjust;
+        this.mHz125 = hz125;
+        this.mHz250 = hz250;
+        this.mHz500 = hz500;
+        this.mHz1000 = hz1000;
+        this.mHz2000 = hz2000;
+        this.mHz3000 = hz3000;
+        this.mHz4000 = hz4000;
+        this.mHz8000 = hz8000;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public int getVolumeAdjust() {
+        return mVolumeAdjust;
+    }
+
+    public void setVolumeAdjust(int mVolumeAdjust) {
+        this.mVolumeAdjust = mVolumeAdjust;
+    }
+
+    public short getHz125() {
+        return mHz125;
+    }
+
+    public void setHz125(short mHz125) {
+        this.mHz125 = mHz125;
+    }
+
+    public short getHz250() {
+        return mHz250;
+    }
+
+    public void setHz250(short mHz250) {
+        this.mHz250 = mHz250;
+    }
+
+    public short getHz500() {
+        return mHz500;
+    }
+
+    public void setHz500(short mHz500) {
+        this.mHz500 = mHz500;
+    }
+
+    public short getHz1000() {
+        return mHz1000;
+    }
+
+    public void setHz1000(short mHz1000) {
+        this.mHz1000 = mHz1000;
+    }
+
+    public short getHz2000() {
+        return mHz2000;
+    }
+
+    public void setHz2000(short mHz2000) {
+        this.mHz2000 = mHz2000;
+    }
+
+    public short getHz3000() {
+        return mHz3000;
+    }
+
+    public void setHz3000(short mHz3000) {
+        this.mHz3000 = mHz3000;
+    }
+
+    public short getHz4000() {
+        return mHz4000;
+    }
+
+    public void setHz4000(short mHz4000) {
+        this.mHz4000 = mHz4000;
+    }
+
+    public short getHz8000() {
+        return mHz8000;
+    }
+
+    public void setHz8000(short mHz8000) {
+        this.mHz8000 = mHz8000;
+    }
+
+}
---
+++ app/src/main/java/peterson/ttu/edu/backupaids/model/SoundPresetManager.java
@@ -0,0 +1,96 @@
+package peterson.ttu.edu.backupaids.model;
+
+import android.content.Context;
+import android.util.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import peterson.ttu.edu.backupaids.R;
+
+/**
+ * Controls access to the presets
+ */
+
+public class SoundPresetManager {
+
+
+    private static SoundPresetManager smInstance;
+
+    private Map<String, SoundPreset> mPresets = new HashMap<>();
+
+    public static SoundPresetManager getInstance(Context context) {
+        if ( smInstance == null) {
+            synchronized (SoundPresetManager.class) {
+                if ( smInstance == null) {
+                    smInstance = new SoundPresetManager(context);
+                }
+            }
+        }
+        return smInstance;
+    }
+
+    private final Context mContext;
+
+    private SoundPresetManager(Context context) {
+        mContext = context;
+        JsonReader jsonReader = null;
+        try {
+            InputStream inputStream = context.getAssets().open(context.getString(R.string.preset_file_name));
+            jsonReader = new JsonReader(new InputStreamReader(inputStream));
+            readAllPresets(jsonReader);
+        } catch (FileNotFoundException e) {
+            // Okay, just no presets
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO: handle this better...
+        } finally {
+            if ( jsonReader != null) {
+                try {
+                    jsonReader.close();
+                } catch ( Exception e) {
+                    // We tried...
+                }
+            }
+        }
+
+    }
+
+    private void readAllPresets(JsonReader jsonReader) throws IOException {
+        while(jsonReader.hasNext()) {
+            SoundPreset preset = new SoundPreset(jsonReader);
+            mPresets.put(preset.getName(), preset);
+        }
+    }
+
+    public void addPreset(SoundPreset preset) {
+        mPresets.put(preset.getName(), preset);
+        savePresets();
+    }
+
+    /**
+     * Call this when you're ready to save changes to 1+ presets
+     */
+    public void savePresets() {
+
+    }
+
+    public List<String> getSortedPresetNames() {
+        List<String> list = new ArrayList<>(mPresets.keySet());
+        Collections.sort(list);
+        return list;
+    }
+
+    public SoundPreset getPreset(String name) {
+        return mPresets.get(name);
+    }
+}
