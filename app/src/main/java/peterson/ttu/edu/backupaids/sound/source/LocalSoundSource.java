4dc4e3a9ff666b23a89eb9a865a77519381faf07
Erik Peterson <erik.peterson@ttu.edu>
2019-05-30 08:49:13 -0500

    Found it's better to use a 1/2-size buffer to reduce latency....

--- app/src/main/java/peterson/ttu/edu/backupaids/sound/source/LocalSoundSource.java
+++ app/src/main/java/peterson/ttu/edu/backupaids/sound/source/LocalSoundSource.java
@@ -9,6 +9,8 @@ import peterson.ttu.edu.backupaids.util.Util;
  */
 public class LocalSoundSource implements SoundSource {
 
+    private static final String TAG = "LocalSoundSource";
+
     private final AudioRecord audioRecord;
 
     private boolean firstRead = true;
@@ -36,54 +38,9 @@ public class LocalSoundSource implements SoundSource {
             // What we're trying to do here is establish the amount of time it takes to get data
             firstRead = false;
             // Dump everything in the first read so we have as little latency as possible
-            audioRecord.read(buffer, 0, buffer.length, AudioRecord.READ_NON_BLOCKING);
-            long startTime = System.nanoTime();
-            while ( true) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    // Okay, just check again
-                }
-                int amountRead = audioRecord.read(buffer, 0, buffer.length, AudioRecord.READ_NON_BLOCKING);
-                if ( amountRead != 0) {
-                    lastReadTime = System.nanoTime();
-                    // Shave off any sub-millisecond data so we
-                    sleeptime = (int) (Math.floor((lastReadTime - startTime) / 1000000));
-                    return amountRead;
-                }
-            }
+            audioRecord.read(buffer, 0, buffer.length);
         }
-
-        // Getting here means this isn't the first time to read. Use the saved time to sleep until
-        // just before we should have data.
-        // Account for any delay since the last read just in case a GC run happened or something
-        int amountToSleep = (int) (sleeptime - (System.nanoTime() - lastReadTime));
-        try {
-            Thread.sleep(amountToSleep);
-        } catch ( InterruptedException e) {
-            // Okay, just start checking
-        }
-
-        // There shouldn't be any data in this first check because we intentionally woke up early
-        // (whatever sub-millisecond leftover that got rounded down in the Math.floor above).
-        // If there is data now there was a delay in that first reading. Back off a millisecond
-        // for the next read.
-        int amountRead = audioRecord.read(buffer, 0, buffer.length, AudioRecord.READ_NON_BLOCKING);
-        if ( amountRead < 0) {
-            return amountRead;
-        } else if ( amountRead > 0) {
-            sleeptime--;
-            return amountRead;
-        }
-
-        // Getting here means that, as expected, we don't have any data yet. Repeatedly check until we get something
-        while ( true) {
-            amountRead = audioRecord.read(buffer, 0, buffer.length, AudioRecord.READ_NON_BLOCKING);
-            if ( amountRead != 0) {
-                lastReadTime = System.nanoTime();
-                return amountRead;
-            }
-        }
+        return audioRecord.read(buffer, 0, buffer.length / 2);
     }
 
     @Override
